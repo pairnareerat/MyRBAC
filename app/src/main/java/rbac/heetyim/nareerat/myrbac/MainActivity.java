26bd2941867ea5dfded5f37e0e238a2b11405c63
pair <parstar@hotmail.com>
2017-08-05 14:25:34 +0700

    before Lunch

--- app/src/main/java/rbac/heetyim/nareerat/myrbac/MainActivity.java
+++ app/src/main/java/rbac/heetyim/nareerat/myrbac/MainActivity.java
@@ -3,11 +3,22 @@ package rbac.heetyim.nareerat.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rbac.heetyim.nareerat.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add fragment
+        if (savedInstanceState == null){
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, mainFragment)
+                    .commit();
+        }
+    }// Main Method
+}//Main Class
